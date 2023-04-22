de4812700a7b5e13dbd3e29ac7a82c0112fe3fee
goenning <me@goenning.net>
2023-04-22 18:59:34 +0100

    add some more notes

--- lib/frameworks/nativescript.ts
+++ lib/frameworks/nativescript.ts
@@ -4,8 +4,30 @@ export const nativescript: Framework = {
   slug: "nativescript",
   name: "Native Script",
   description: "Build truly native apps with JavaScript.",
-  pros: [],
-  cons: [],
+  pros: [
+    {
+      title: "Cross-platform",
+      description:
+        "Native Script allows developers to build mobile apps for multiple platforms, including Android and iOS, using a single codebase.",
+    },
+    {
+      title: "Native APIs",
+      description:
+        "Native Script provides a bridge to all native APIs via JavaScript, without having to write anything in Swift or Kotlin. This means that developers can access native functionality such as camera, contacts, and file storage.",
+    },
+  ],
+  cons: [
+    {
+      title: "Performance",
+      description:
+        "Native Script uses JavaScript as a bridge to Native APIs, which is limited to a single-thread and may not perform as well as those built natively.",
+    },
+    {
+      title: "Emerging Community",
+      description:
+        "Despite being around for a while, the Native Script is not as popular as other frameworks, which means that there is not a lot of documentation, tutorials, and examples available to help developers apps.",
+    },
+  ],
   releases: {
     first: "April 29, 2015",
   },
