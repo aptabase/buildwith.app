95ffe07c91c67252629951cf08ddff96955b551e
goenning <me@goenning.net>
2023-04-22 07:36:48 +0100

    pros cons of some

--- lib/frameworks/android.ts
+++ lib/frameworks/android.ts
@@ -4,8 +4,40 @@ export const android: Framework = {
   slug: "android",
   name: "Android (Native)",
   description: "Build truly native apps for Android devices.",
-  pros: [],
-  cons: [],
+  pros: [
+    {
+      title: "Official SDK",
+      description:
+        "Native apps are built using the official Android development tools, which provide access to the latest features and functionality of the Android platform.",
+    },
+    {
+      title: "Performance",
+      description:
+        "Native Android apps perform better than hybrid apps as they have direct access to device hardware and the underlying operating system.",
+    },
+    {
+      title: "User Experience",
+      description:
+        "Native Android apps offer a consistent user experience that users are familiar with.",
+    },
+    {
+      title: "Ecosystem",
+      description:
+        "Developers can build apps for other Android-based systems like Smartphones, Wear OS, Android TV and Android Auto.",
+    },
+    {
+      title: "Large Community",
+      description:
+        "Android has a very large and active developer community, which means there is plenty of documentation, tutorials, and examples available to help developers build apps for Android.",
+    },
+  ],
+  cons: [
+    {
+      title: "Cross-platform",
+      description:
+        "Developers have to build separate apps using a different framework to target other platforms like iOS, Windows and macOS.",
+    },
+  ],
   href: "https://developer.android.com/",
   logoUrl: "https://aptabase.com/tools/android.svg",
   languages: ["Java", "Kotlin"],
--- lib/frameworks/apple.ts
+++ lib/frameworks/apple.ts
@@ -4,8 +4,40 @@ export const apple: Framework = {
   slug: "apple",
   name: "Apple (Native)",
   description: "Build truly native apps for Apple devices using Swift.",
-  pros: [],
-  cons: [],
+  pros: [
+    {
+      title: "Official SDK",
+      description:
+        "Native apps are built using the official Apple development tools, which provide access to the latest features and functionality of the Apple ecosystem.",
+    },
+    {
+      title: "Performance",
+      description:
+        "Native Apple apps perform better than hybrid apps as they have direct access to device hardware and the underlying operating system.",
+    },
+    {
+      title: "User Experience",
+      description:
+        "Native Apple apps offer a consistent user experience that users are familiar with.",
+    },
+    {
+      title: "Ecosystem",
+      description:
+        "Developers can build apps for other all Apple products like Apple Watch, Apple TV, Mac, iPad and iPhone.",
+    },
+    {
+      title: "Large Community",
+      description:
+        "Apple has a very large and active developer community, which means there is plenty of documentation, tutorials, and examples available to help developers build apps for Apple.",
+    },
+  ],
+  cons: [
+    {
+      title: "Cross-platform",
+      description:
+        "Developers have to build separate apps using a different framework to target other platforms like Android and Windows.",
+    },
+  ],
   href: "https://developer.apple.com/",
   logoUrl: "https://aptabase.com/tools/apple.svg",
   languages: ["Swift"],
