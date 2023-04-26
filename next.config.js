1a9df2ee764cf1ae54caff9536b4a849afc70429
goenning <me@goenning.net>
2023-04-26 05:59:01 +0100

    disable scrollRestoration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    scrollRestoration: false,
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
